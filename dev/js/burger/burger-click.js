df78cefcbbb7da112c4dbb785d542958f5b44063
Eric Lauterbach <efl32@drexel.edu>
2020-01-26 18:17:47 -0500

    header grid fix

--- dev/js/burger/burger-click.js
+++ dev/js/burger/burger-click.js
@@ -1,7 +1,7 @@
 var linesToArrow = gsap.timeline({paused: true});
 
-linesToArrow.to("#top-line", {duration: 0.25, rotate: -45, transformOrigin: "100%, 50%", x: -8, y: 2},"linesArrow")
-.to("#bottom-line", {duration: 0.25, rotate: 45, transformOrigin: "100%, 50%", x: -8, y: -2},"linesArrow")
+linesToArrow.to("#top-line", {duration: .25, rotate: -45, transformOrigin: "100%, 50%", x: -8, y: 2},"linesArrow")
+.to("#bottom-line", {duration: .25, rotate: 45, transformOrigin: "100%, 50%", x: -8, y: -2},"linesArrow")
 .to("#burgerContainer", {duration: .25, scaleX: -1}, "linesArrow");
 
 
