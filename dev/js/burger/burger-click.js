0086b3d036654e9136d8e7a9b5e8f44dc2776144
Eric Lauterbach <efl32@drexel.edu>
2020-01-30 14:09:21 -0500

    take-over nav

--- dev/js/burger/burger-click.js
+++ dev/js/burger/burger-click.js
@@ -11,11 +11,16 @@ $("#burgerContainer").on("click", function(){
 
     if(canYouSeeNav === false){
         linesToArrow.play();
-        gsap.to("#navBG", { duration: 0.5, scale: 100, alpha: 0.85});
+        gsap.to("#navBG", { duration: 0.5, scale: 100, alpha: 0.9});
+        gsap.set("nav", { display: "flex"});
+        gsap.from("nav li",{duration:0.5, alpha:0, stagger:0.15, delay:0.25, y:50});
+
+
         canYouSeeNav = true;
     }else{
         linesToArrow.reverse();
-        gsap.to("#navBG", { duration: 0.5, scale: 1});
+        gsap.to("#navBG",{duration:0.25, scale:1});
+        gsap.to("nav li",{duration:0.25, alpha:0, onComplete:hideNav});
         canYouSeeNav = false;
     }
 
--- dev/js/burger/burger.js
+++ dev/js/burger/burger.js
@@ -4,3 +4,8 @@ gsap.set("#navBG", {transformOrigin: "50% 50%"})
 
 // set lines to rotate over center
 gsap.set(".burgerLines", {transformOrigin: "50%, 50%"});
+
+function hideNav(){
+    gsap.set("nav",{display:"none"});
+    gsap.set("nav li",{alpha:1});
+}
